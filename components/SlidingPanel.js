040906dcb26ccf3240a4f47c648f118cbf69035d
Yash Gupta <yashguptaz@protonmail.com>
2021-08-15 10:52:04 +0530

    wip: slide images

---
+++ components/SlidingPanel.js
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+
+const Section = styled.section`
+  display: inline-block;
+  overflow: hidden;
+  margin-left: 10px;
+
+  img {
+    width: 350px;
+    height: 270px;
+    border-style: solid;
+    display: inline-block;
+  }
+
+  .slide {
+    position: relative;
+    display: inline-block;
+    overflow: hidden;
+  }
+  .slide img {
+    display: inline-block;
+    vertical-align: middle;
+  }
+  .slide-content {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    padding: 20px;
+    box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+    background-color: rgba(168, 102, 57, 0.9);
+    color: #ffffff;
+    transition: transform 0.3s ease;
+    -webkit-transition: transform 0.3s ease;
+  }
+  .slide:hover .slide-content {
+    transform: translate(0, 0);
+    -webkit-transform: translate(0, 0);
+  }
+
+  .slide-bottom .slide-content {
+    transform: translate(0, 100%);
+    -webkit-transform: translate(0, 100%);
+  }
+`;
+
+const SlidingPanel = ({ name, children }) => (
+  <Section>
+    <div className="slide slide-bottom">
+      <div className="slide-content">{name}</div>
+      {children}
+    </div>
+  </Section>
+);
+
+export default SlidingPanel;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Header from "../components/header";
 import Button from "../components/button";
+import SlidingPanel from "../components/SlidingPanel";
 import styles from "../styles/Home.module.css";
 
 import Image from "next/image";
@@ -11,7 +12,9 @@ export default function Home() {
         <div>
           <h1>NoWaste</h1>
           <p>
-            An all in one recycling app for everyone to figure out how they can help reduce the overflowing landfills in our ecosystem and prevent the environment from getting destroyed. {" "}
+            An all in one recycling app for everyone to figure out how they can
+            help reduce the overflowing landfills in our ecosystem and prevent
+            the environment from getting destroyed.{" "}
           </p>
           <Button>Learn More</Button>
         </div>
@@ -24,32 +27,56 @@ export default function Home() {
           />
         </div>
       </Header>
+
+      {/* Features */}
       <div className={styles.features}>
         <h1>Features</h1>
-        <p>NoWaste provides a multitude of ways for people to help reduce waste in their communities by recycling, donating, and substituting things.</p>
-          <div className={styles.card}>
-            <div className={styles.card}>
-              <span>Carbon Footprints</span>
-              <Image src="/imgs/cfoot.png" width="200px" height="200px" alt="CarbonFootprints"/>
-            </div>
-            <div className={styles.card}>
-              <span>E-shop</span>
-              <Image src="/imgs/eshop.png" width="170" height="150" alt="EShop"/>
-            </div>
-            <div className={styles.card}>
-              <span>Chat bot</span>
-              <Image src="/imgs/chatbot.png" width="380px" height="300px" alt="ChatBot"/>
-            </div>
-            <div className={styles.card}>
-              <span>Donate food</span>
-              <Image src="/imgs/reuse.png" width="200px" height="200px" alt="Donatefood"/>
-            </div>
-            <div className={styles.card}>
-              <span>Recycle</span>
-              <Image src="/imgs/recycle.png" width="150px" height="150px" alt="Recycle"/>
-            </div>
-          </div>
-      </div>      
+        <p>
+          NoWaste provides a multitude of ways for people to help reduce waste
+          in their communities by recycling, donating, and substituting things.
+        </p>
+        <div className={styles.card}>
+          <SlidingPanel name="Carbon Footprints">
+            <Image
+              src="/imgs/cfoot.png"
+              width="200px"
+              height="200px"
+              alt="CarbonFootprints"
+            />
+          </SlidingPanel>
+
+          <SlidingPanel name="E-shop">
+            <Image src="/imgs/eshop.png" width="170" height="150" alt="EShop" />
+          </SlidingPanel>
+
+          <SlidingPanel name="Chat Bot">
+            <Image
+              src="/imgs/chatbot.png"
+              width="380px"
+              height="300px"
+              alt="ChatBot"
+            />
+          </SlidingPanel>
+
+          <SlidingPanel name="Donate Food">
+            <Image
+              src="/imgs/reuse.png"
+              width="200px"
+              height="200px"
+              alt="Donatefood"
+            />
+          </SlidingPanel>
+
+          <SlidingPanel name="Recycle">
+            <Image
+              src="/imgs/recycle.png"
+              width="150px"
+              height="150px"
+              alt="Recycle"
+            />
+          </SlidingPanel>
+        </div>
+      </div>
     </div>
   );
 }
